e82713295baba9f75a8f6f5873842578dd7352ff
pado8 <floe9235@gmail.com>
2025-06-04 16:22:37 +0900

    0604 react 강의 내용 커밋

--- reactjs_workspace/frontend/mall/src/router/root.js
+++ reactjs_workspace/frontend/mall/src/router/root.js
@@ -1,20 +1,20 @@
-import { lazy, Suspense } from "react";
-
+import { Suspense, lazy } from "react";
+import todoRouter from "./todoRouter";
 const { createBrowserRouter } = require("react-router-dom");
-
-const Loading = <div>Loading...</div>
-const Main = lazy(()=> import("../pages/MainPage"));
-const About = lazy(()=> import("../pages/AboutPage"));
-
-
+const Loading = <div>Loading....</div>
+const Main = lazy(() => import("../pages/MainPage"))
+const About = lazy(() => import("../pages/AboutPage"))
+const TodoIndex = lazy(() => import("../pages/todo/IndexPage"))
 const root = createBrowserRouter([
     {
-        path: "",
-        element: <Suspense fallback={Loading}><Main /></Suspense>,
+        path: "", element: <Suspense fallback={Loading}><Main /></Suspense>
     },
     {
-        path: "about",
-        element: <Suspense fallback={Loading}><About /></Suspense>,
+        path: "about", element: <Suspense fallback={Loading}><About /></Suspense>
+    },
+    {
+        path: "todo", element: <Suspense fallback={Loading}><TodoIndex /></Suspense>,
+        children: todoRouter()
     }
 ])
 export default root;
