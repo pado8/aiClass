37420ffc32d8e683b7fb50c94f20b70295281737
pado8 <floe9235@gmail.com>
2025-06-05 13:56:57 +0900

    0605 react + spring boot 강의 내용 커밋

--- reactjs_workspace/frontend/mall/src/pages/todo/IndexPage.js
+++ reactjs_workspace/frontend/mall/src/pages/todo/IndexPage.js
@@ -16,9 +16,9 @@ const IndexPage = () => {
     return (
         <BasicLayout>
             <div className="w-full flex m-2 p-2 ">
-                <div className="text-xl m-1 p-2 w-20 font-extrabold text-center underline"
+                <div className="my-btn"
                     onClick={handleClickList}> LIST </div>
-                <div className="text-xl m-1 p-2 w-20 font-extrabold text-center underline"
+                <div className="my-btn"
                     onClick={handleClickAdd}> ADD </div>
             </div>
             <div className="flex flex-wrap w-full"> <Outlet /> </div>
